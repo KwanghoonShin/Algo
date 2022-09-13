2b1fe9c765ea00a4ece9a48854f26470f36942e4
KwanghoonShin <rhkdskh456@gmail.com>
2022-09-14 07:45:18 +0900

    Law of large numbers

---
+++ Number_Card_Game.py
@@ -0,0 +1,21 @@
+N,M = map(int, input().split())
+
+array = list()
+result = 0
+
+for _ in range(N):
+    array.append(list(map(int, input().split())))
+
+# choose Raw
+min_raw = 0
+min_raw = min(array[0])
+ch_raw = 0
+
+for i in range(N):
+    if min_raw <= min(array[i]):
+        min_raw = min(array[i])
+        ch_raw = i + 1
+
+# choose maximum value
+result = min(array[ch_raw - 1])
+print(result)
